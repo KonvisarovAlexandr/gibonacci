397ba497ec7d570c3b5d8f061822eecdd6827f83
Konvisarov <alexandr.convisarov@isa.utm.md>
2023-04-14 16:02:24 +0300

    Initial Commit

---
+++ Fibonacci/FibonacciApp.swift
@@ -0,0 +1,17 @@
+//
+//  FibonacciApp.swift
+//  Fibonacci
+//
+//  Created by Alexandr on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FibonacciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
